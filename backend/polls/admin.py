6f1a58f49147c487e39b2f2ce3e352a07e5e7e02
Christian <winterdevelopers@gmail.com>
2023-08-22 23:38:47 +0100

    tried fixing nginx error, admin formatting, contender styling

--- backend/polls/admin.py
+++ backend/polls/admin.py
@@ -5,10 +5,13 @@ class CandidateFormat(admin.ModelAdmin):
     list_display=('name','votes','poll')
     prepopulated_fields={'slug':('name','poll')}
 
+class EmailVotingFormat(admin.ModelAdmin):
+    list_display=('amount','candidate','reference','transaction_status')
+    search_fields = ['reference', 'amount']
 
 # Register your models here.
 admin.site.register(Poll)
 admin.site.register(Candidate,CandidateFormat)
 admin.site.register(Office)
-admin.site.register(EmailPayment)
+admin.site.register(EmailPayment,EmailVotingFormat)
 admin.site.register(VotingCoupon)
